7c1893d6fa84948690513524b003cb41baf0cf13
Fomafix <fomafix@googlemail.com>
2024-01-02 16:59:40 +0000

    Mark tag 'redirectmanager' as active software defined tag
    This is visible on Special:Tags.
    
    Change-Id: Ic41a27b422cc00f1294ddb9bc4a95d868fcece16

--- src/Hooks.php
+++ src/Hooks.php
@@ -2,9 +2,15 @@
 
 namespace MediaWiki\Extension\RedirectManager;
 
+use MediaWiki\ChangeTags\Hook\ChangeTagsListActiveHook;
+use MediaWiki\ChangeTags\Hook\ListDefinedTagsHook;
 use MediaWiki\Hook\EditPage__showEditForm_initialHook;
 
-class Hooks implements EditPage__showEditForm_initialHook {
+class Hooks implements
+	EditPage__showEditForm_initialHook,
+	ListDefinedTagsHook,
+	ChangeTagsListActiveHook
+{
 
 	// phpcs:disable MediaWiki.NamingConventions.LowerCamelFunctionsName.FunctionName
 
@@ -13,5 +19,15 @@ class Hooks implements EditPage__showEditForm_initialHook {
 	 */
 	public function onEditPage__showEditForm_initial( $editor, $out ) {
 		$out->addModules( [ 'ext.RedirectManager', 'ext.RedirectManager.images' ] );
+	}
+
+	/** @inheritDoc */
+	public function onListDefinedTags( &$tags ): void {
+		$tags[] = 'redirectmanager';
+	}
+
+	/** @inheritDoc */
+	public function onChangeTagsListActive( &$tags ): void {
+		$tags[] = 'redirectmanager';
 	}
 }
